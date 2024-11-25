4e7639f27bff3bb4021df8bf433534c7ded9f427
Devanyrdz12 <devany.rodriguez.alp@cbtis258.edu.mx>
2024-11-25 10:15:28 -0600

    Update ww.js

---
+++ ww.js
@@ -0,0 +1,11 @@
+let orden_correcto = ['when', 'while', 'when', 'while'];
+
+let palabras_juego = [ 'while','when','when','while'];
+
+let contenedorOpciones = document.getElementById("opciones");
+
+let comprobar = document.getElementById("comprobar");
+
+let txtResultado = document.getElementById("resultado");
+
+let posDisponible = ["","","","",]
